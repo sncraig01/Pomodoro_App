c3e330a50f6a6eee736e7fc8ac6d2946d8953925
camillemcooper <48187933+camillemcooper@users.noreply.github.com>
2019-05-28 17:07:56 -0400

    made super basic timer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Timer from "./timer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Sarah changed this
+        <Timer />
       </header>
     </div>
   );
