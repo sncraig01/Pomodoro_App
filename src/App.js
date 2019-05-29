0c0e085fe5bdd28f09feefbb6e4b54e74d9e50ba
camillemcooper <48187933+camillemcooper@users.noreply.github.com>
2019-05-29 16:24:04 -0400

    css fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Timer from "./timer";
 import React from "react";
 import "./App.css";
-import AuthUser from "./authUser.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">       
+      <header className="App-header">
         <Timer />
       </header>
     </div>
