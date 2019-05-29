8a149433083fa2e58f1773f2eec02f984c8cde74
Brock Gardner <brockgardner@d-172-25-230-145.dhcp.virginia.edu>
2019-05-29 08:41:29 -0400

    Added login and routing components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AuthUser from "./authUser.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Sarah changed this
-      </header>
+      <h1>Login Success!</h1>
     </div>
   );
 }
