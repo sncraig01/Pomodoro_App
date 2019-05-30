c2adcb9d4283ecc5308a2a7156a3b76da3c6686d
Brock Gardner <brockgardner@d-172-25-105-42.dhcp.virginia.edu>
2019-05-30 12:02:57 -0400

    Synchronized button sequence

--- src/breakTimer.js
+++ src/breakTimer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./timer.css";
+import RandGen from "./randGen.js";
 
 class BreakTimer extends Component {
   state = {
@@ -31,7 +32,13 @@ class BreakTimer extends Component {
     let minutes = ("0" + Math.floor((this.state.timerTime / 60000) % 60)).slice(
       -2
     );
-
+    if (this.state.timerTime == 0) {
+      return (
+        <button type="primary" onClick={this.props.doItAgainClicked}>
+          Do it Again!
+        </button>
+      );
+    }
     return (
       //the return starts with a ternany operator that checks whether we want to collect input or not. if we do it calls up
       //InputForm.js and if not it has the timer set up to run. The timer has a bunch of logic that looks at all the state conditions
@@ -41,6 +48,8 @@ class BreakTimer extends Component {
         <h2 type="timer" style={{ color: "white" }}>
           {minutes} : {seconds}
         </h2>
+        <p type="inputform">You deserve a break!</p>
+        <RandGen />
       </div>
     );
   }
