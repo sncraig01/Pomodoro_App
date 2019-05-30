40188b6a9131731662037cd6f0700e8d961f892f
Brock Gardner <brockgardner@d-172-25-105-42.dhcp.virginia.edu>
2019-05-30 15:47:48 -0400

    minor changes

--- src/Profile.js
+++ src/Profile.js
@@ -4,66 +4,69 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import SvgIcon from "@material-ui/core/SvgIcon";
-import "./App.css"
+import "./App.css";
 import "./Profile.css";
-import Graph from "./Graph.js"
+import Graph from "./Graph.js";
 import firebase from "./Firebase.js";
-import { Row, Col } from 'antd';
+import { Row, Col } from "antd";
 
 export default class Profile extends React.Component {
-
-  state={
+  state = {
     name: "",
     email: "",
 
     activities: [],
     num_activities_today: 0,
-    activities_count: 0,
-  }
+    activities_count: 0
+  };
 
-  componentDidMount=()=>{
+  componentDidMount = () => {
     // set the state with the current user info
     if (firebase.auth().currentUser) {
       //get the current user, if its not null
       var user = firebase.auth().currentUser;
       this.setState({ name: user.displayName, email: user.email }); //set the state with the info
 
-      const activitiesRef = firebase.database().ref("users/" + user.displayName ); //reference to the database, for the specific user
+      const activitiesRef = firebase
+        .database()
+        .ref("users/" + user.displayName); //reference to the database, for the specific user
       //do something with the data
-      activitiesRef.on("value", (snapshot) => {
-        console.log( "snapshot")
-        console.log( snapshot.val() );
-  
-        var returnArr = [];
-        snapshot.forEach(function(childSnapshot) { // for each activity
+      activitiesRef.on(
+        "value",
+        snapshot => {
+          console.log("snapshot");
+          console.log(snapshot.val());
+
+          var returnArr = [];
+          snapshot.forEach(function(childSnapshot) {
+            // for each activity
             var item = childSnapshot.val();
-    
+
             returnArr.push(item); //add it to this array
-        });
-  
-       this.setState( {activities: returnArr } );
+          });
 
-       this.getTodaysStats();
-       
-    }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-    });
+          this.setState({ activities: returnArr });
+
+          this.getTodaysStats();
+        },
+        function(errorObject) {
+          console.log("The read failed: " + errorObject.code);
+        }
+      );
     }
-
-  }
+  };
 
   countInArray(array, what) {
     var count = 0;
     for (var i = 0; i < array.length; i++) {
-        if (array[i] === what) {
-            count++;
-        }
+      if (array[i] === what) {
+        count++;
+      }
     }
     return count;
-}
+  }
 
-
-  getTodaysStats=()=>{
+  getTodaysStats = () => {
     let today = new Date(); //get the current date
     const monthNames = [
       "January",
@@ -79,7 +82,12 @@ export default class Profile extends React.Component {
       "November",
       "December"
     ];
-    let todays_date = monthNames[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
+    let todays_date =
+      monthNames[today.getMonth()] +
+      " " +
+      today.getDate() +
+      ", " +
+      today.getFullYear();
 
     let alldata = this.state.activities;
 
@@ -88,46 +96,61 @@ export default class Profile extends React.Component {
     //get an array of all the dates
     let datesALL = [];
 
-    for( let i = 0; i < alldata.length; i ++ ){
-        let testDate = alldata[i].date;
-        datesALL.push( testDate ); //add them all to this one
-        if( !datesONCE.includes( testDate ) ){ //add each one once to this one
-            datesONCE.push( testDate )
-        }
+    for (let i = 0; i < alldata.length; i++) {
+      let testDate = alldata[i].date;
+      datesALL.push(testDate); //add them all to this one
+      if (!datesONCE.includes(testDate)) {
+        //add each one once to this one
+        datesONCE.push(testDate);
+      }
     }
 
-    console.log( "activities completed today: ")
-    let numActivitiesToday = this.countInArray( datesALL, todays_date );
-    this.setState({num_activities_today: numActivitiesToday, activities_count: alldata.length} )
-  }
-
+    console.log("activities completed today: ");
+    let numActivitiesToday = this.countInArray(datesALL, todays_date);
+    this.setState({
+      num_activities_today: numActivitiesToday,
+      activities_count: alldata.length
+    });
+  };
 
   //map all the activities
-  mapItems= () => {
+  mapItems = () => {
     let data = this.state.activities;
-        return data.map( 
-          (item) => 
-          {return   <div> 
-              <Typography align = "center" component="h5" variant="h5" gutterBottom color="inherit">
-                {item.description}
-              </Typography>
-              <Typography align = "center" component="h6" variant="subtitle1" gutterBottom color="inherit" inline>
-                {item.date}
-              </Typography>
-          </div>
-          }
-        )
-  }
+    return data.map(item => {
+      return (
+        <div>
+          <Typography
+            align="center"
+            component="h5"
+            variant="h5"
+            gutterBottom
+            color="inherit"
+          >
+            {item.description}
+          </Typography>
+          <Typography
+            align="center"
+            component="h6"
+            variant="subtitle1"
+            gutterBottom
+            color="inherit"
+            inline
+          >
+            {item.date} @ {item.time}
+          </Typography>
+        </div>
+      );
+    });
+  };
 
   handleClick = e => {
     this.props.history.push("/app");
   };
 
-
   render() {
     return (
-      <div className="Profile" >
-       <AppBar position="static">
+      <div className="Profile">
+        <AppBar position="static">
           <Toolbar>
             <IconButton onClick={this.handleClick} color="inherit">
               <SvgIcon
@@ -144,41 +167,61 @@ export default class Profile extends React.Component {
             </Typography>
           </Toolbar>
         </AppBar>
-        <br/>
-      <Row>
-        <Col span={12}>
-          <Typography component="h2" variant="h2" gutterBottom color="inherit" align="center" >
-            Activity Log
-          </Typography>
-          <div >
-            {this.mapItems()}
-          </div>
-          <br/>
-        </Col>
-        <Col span={1}>
-        </Col>
-        <Col span={10}>
-          <Typography component="h2" variant="h2" gutterBottom color="inherit" align="center">
-            Summary
-          </Typography>
-          <Typography component="h5" variant="h5" gutterBottom color="inherit" align="center">
-            Activities Completed Today: {this.state.num_activities_today}
-          </Typography>
-          <Typography component="h5" variant="h5" gutterBottom color="inherit" align="center" >
-            Total Activities Completed: {this.state.activities_count}
-          </Typography>
-          <br/>
-          <div>
-          {  (this.state.activities.length !== 0) ? 
-                      <Graph data={this.state.activities}/>: null } 
-          </div>
-          <br />
-        </Col>
-        <Col span={1}>
-        </Col>
-      </Row>
+        <br />
+        <Row>
+          <Col span={12}>
+            <Typography
+              component="h2"
+              variant="h2"
+              gutterBottom
+              color="inherit"
+              align="center"
+            >
+              Activity Log
+            </Typography>
+            <div>{this.mapItems()}</div>
+            <br />
+          </Col>
+          <Col span={1} />
+          <Col span={10}>
+            <Typography
+              component="h2"
+              variant="h2"
+              gutterBottom
+              color="inherit"
+              align="center"
+            >
+              Summary
+            </Typography>
+            <Typography
+              component="h5"
+              variant="h5"
+              gutterBottom
+              color="inherit"
+              align="center"
+            >
+              Activities Completed Today: {this.state.num_activities_today}
+            </Typography>
+            <Typography
+              component="h5"
+              variant="h5"
+              gutterBottom
+              color="inherit"
+              align="center"
+            >
+              Total Activities Completed: {this.state.activities_count}
+            </Typography>
+            <br />
+            <div>
+              {this.state.activities.length !== 0 ? (
+                <Graph data={this.state.activities} />
+              ) : null}
+            </div>
+            <br />
+          </Col>
+          <Col span={1} />
+        </Row>
       </div>
     );
   }
 }
-
