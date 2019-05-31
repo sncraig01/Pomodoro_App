7a8fac6b989633192ee6d2cc3b188bc2d2c7fdd3
Sarah  Craig <sarahcraig@d-172-25-231-99.dhcp.virginia.edu>
2019-05-31 10:03:04 -0400

    minor changes

--- src/Graph.js
+++ src/Graph.js
@@ -65,7 +65,7 @@ class Graph extends React.Component {
             <div className="App">
                 <div type="graph"> 
                     {(this.state.formattedData.length !== 0 ) ?     
-                    <VictoryChart domainPadding={50}  >
+                    <VictoryChart domainPadding={10}  >
                         <VictoryLabel text="Your Activity History" x={225} y={30} textAnchor="middle" style={{ fontSize: 30 }} />
                         <VictoryBar data={this.state.formattedData} x="date" y="count"/> 
                     </VictoryChart> : null  }
